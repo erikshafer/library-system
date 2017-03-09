bd778fb600a0c00996c4686d95f36129ddc4487b
erikshafer <erik.shafer@gmail.com>
2017-03-09 10:02:26 -0600

    Initial landing page from Yuchen's code.

---
+++ src/main/java/com/userfront/controller/LandingController.java
@@ -0,0 +1,34 @@
+package com.userfront.controller;
+
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.userfront.dao.RoleDao;
+import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.SavingsAccount;
+import com.userfront.domain.User;
+import com.userfront.domain.security.UserRole;
+import com.userfront.service.UserService;
+
+@Controller
+public class LandingController {
+	
+	@RequestMapping("/landing")
+	public String landing() {
+		return "landing";
+	}
+	
+	@RequestMapping("/welcome")
+    public String welcome() {
+        return "landing";
+    }
+	
+}
